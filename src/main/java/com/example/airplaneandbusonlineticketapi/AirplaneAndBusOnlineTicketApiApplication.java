0a285d72bc4f2681fededfff1e7390d666c2f925
GizemSaricicek <saricicek.gizem07@gmail.com>
2022-07-26 12:42:30 +0300

    spring projesi oluşturuldu.

---
+++ src/main/java/com/example/airplaneandbusonlineticketapi/AirplaneAndBusOnlineTicketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.airplaneandbusonlineticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneAndBusOnlineTicketApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirplaneAndBusOnlineTicketApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airplaneandbusonlineticketapi/AirplaneAndBusOnlineTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airplaneandbusonlineticketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneAndBusOnlineTicketApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
